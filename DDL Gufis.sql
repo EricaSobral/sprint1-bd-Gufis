c1fcee38510270f18150e70a9ce81522672a1cd3
FIC <FIC@info.local>
2020-02-06 11:33:40 -0300

    Subindo aqrquivos

---
+++ DDL Gufis.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE Gufi;
+
+--Define o banco de dados que será utilizado
+USE Gufi;
+
+-- Criação de tabeas
+CREATE TABLE TipoUsuario(
+idTipoUsuario INT PRIMARY KEY IDENTITY,
+TituloTipoUsuario VARCHAR (500) NOT NULL UNIQUE
+);
+
+CREATE TABLE TipoEvento(
+idTipoEvento INT PRIMARY KEY IDENTITY,
+TituloTipoEvento VARCHAR (255) NOT NULL UNIQUE
+);
+
+CREATE TABLE Instituicao (
+idInstituicao INT PRIMARY KEY IDENTITY,
+CNPJ CHAR (14) NOT NULL UNIQUE,
+NomeFantasia VARCHAR (255) NOT NULL UNIQUE,
+Endereco VARCHAR (255) NOT NULL UNIQUE
+);
+
+CREATE TABLE Usuario(
+idUsuario INT PRIMARY KEY IDENTITY,
+NomeUsuario VARCHAR(255) NOT NULL,
+Email VARCHAR (100) NOT NULL UNIQUE,
+Senha VARCHAR (100) NOT NULL,
+DataCadastro DATETIME2 NOT NULL, 
+Genero VARCHAR (255),
+idTipoUsuario INT FOREIGN KEY REFERENCES TipoUsuario (idTipoUsuario)
+);
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # sprint1-bd-Gufis
+# sprint1-bd-Gufis # sprint1-bd-Gufis
